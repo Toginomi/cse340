182e1fcab785fb23303ddc590eda8bdb7413e99d
Toginomi <rudyiipilande@gmail.com>
2025-09-25 12:26:52 +0800

    added intentional error

--- controllers/invController.js
+++ controllers/invController.js
@@ -7,31 +7,48 @@ const invCont = {}
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
-  res.render("./inventory/classification", {
-    title: className + " vehicles",
-    nav,
-    grid,
-  })
+  try{
+    const classification_id = req.params.classificationId
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+      title: className + " vehicles",
+      nav,
+      grid,
+    })
+  } catch (error){
+    next (error)
+  }
 }
 
 /* ***************************
  *  Build vehicle details view
  * ************************** */
 invCont.buildByInventoryId = async function (req, res, next){
-  const inventory_id = req.params.inventoryId
-  const data = await invModel.getDetailsByInventoryId(inventory_id)
-  let nav = await utilities.getNav()
-  const title = `${data.inv_make} ${data.inv_model}`
-  res.render("./inventory/detail", {
-    title,
-    nav,
-    vehicle: data
-  })
+  try{
+    const inventory_id = req.params.inventoryId
+    const data = await invModel.getDetailsByInventoryId(inventory_id)
+    let nav = await utilities.getNav()
+    const title = `${data.inv_make} ${data.inv_model}`
+    res.render("./inventory/detail", {
+      title,
+      nav,
+      vehicle: data
+    })
+  } catch (error){
+    next(error)
+  }
+}
+
+/* ***************************
+ *  Build intentional error view
+ * ************************** */
+invCont.triggerError = function(req, res, next){
+  const err = new Error("This is an intentional error for testing purposes.")
+  err.status = 500
+  next(err)
 }
 
 module.exports = invCont
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,11 +2,15 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 // Route to build vehicle details view
-router.get("/detail/:inventoryId", invController.buildByInventoryId)
+router.get("/detail/:inventoryId", utilities.handleErrors(invController.buildByInventoryId))
+
+// Route to trigger intentional error view
+router.get("/trigger-error", utilities.handleErrors(invController.triggerError))
 
 module.exports = router;
